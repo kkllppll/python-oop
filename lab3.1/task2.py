412011d5702870ef663fe78371b5de859e55e0b3
Elina Sharabura <113701591+kkllppll@users.noreply.github.com>
2023-01-20 15:00:16 +0200

    Update task2.py

--- lab3.1/task2.py
+++ lab3.1/task2.py
@@ -6,16 +6,17 @@ class Calendar:
         self.month_last = None
 
     def subtracting(self, days=0, months=0, years=0):
-        start_day = self.date.day
-        start_year = self.date.year
+
 
         if self.date.month - months > 0: self.month_last = self.date.month - months
         else:
             self.month_last = 12 - abs((self.date.month - months))
 
+        start_year = self.date.year
         while (self.date.month != self.month_last) or (self.date.year != (start_year - years)):
             self.date -= datetime.timedelta(days=1)
 
+        start_day = self.date.day
         last_month_day = self.date.day
         self.date -= datetime.timedelta(days=last_month_day-start_day+days)
 
@@ -27,11 +28,11 @@ class Calendar:
         else:
             self.month_last = 12
 
-        start_day = self.date.day
         start_year = self.date.year
         while (self.date.month != self.month_last) or (self.date.year != (start_year + years)):
             self.date += datetime.timedelta(days=1)
 
+        start_day = self.date.day
         for i in range(start_day+days-1): 
             self.date += datetime.timedelta(days=1)
 
@@ -40,6 +41,8 @@ class Calendar:
    
 
 def change_date():
+    
+    number = input("Your choice -->") 
     number_day = input("Since you want to change the date you need to enter some information\n \
         enter the number of days, months, years for which you want to change the date.\n Enter the days --> ")
     number_month = input("Month --> ")
@@ -57,24 +60,23 @@ def change_date():
 
 
 calendar = Calendar()
-number = input("Date: Today`s date (press 1), Your own date (press 2)")
+number = input("Date: Today`s date (press 1), Your own date (press 2)\n Your choice -->")
 if number in ["1", "2"]:
         if number == "1": print("Today is", calendar.date)
         elif number == "2":
             day = input("Enter:\n day --> ")
-            month = input("month --> ")
+            month = input("months --> ")
             year = input("year -->")
             datetime.datetime.strptime(day + "/" + month + "/" + year, "%d/%m/%Y").date()
             print("Your date:", calendar.date)
         else:
             calendar = Calendar()
-
+        print("For adding days to the date press --> 1, for subtracting press --> 2?")
         func = change_date()
         change_day = int(func[1]) 
         change_month = int(func[2])
         change_year = int(func[3])
-        print("For adding days to the date press --> 1, for subtracting press --> 2?")
-        number = input("Your choice -->")
+        
         if func[0] == 1:
                 print(f"After {change_day} day(s), {change_month} month(s), {change_year} year(s) will be:",
                   calendar.adding(days=change_day, months=change_month, years=change_year))
@@ -90,18 +92,14 @@ if number in ["1", "2"]:
         elif func[0] == 2:
             print(f"{change_day} day(s), {change_month} month(s), {change_year} year(s) before was:",
                 calendar.subtracting(days=change_day, months=change_month, years=change_year))
-            if calendar.date !=  calendar.adding(days=change_day, months=change_month, years=change_year):
-                if  calendar.date >  calendar.adding(days=change_day, months=change_month, years=change_year):
+            if calendar.date !=  calendar.subtracting(days=change_day, months=change_month, years=change_year):
+                if  calendar.date >  calendar.subtracting(days=change_day, months=change_month, years=change_year):
                         print(f'\t\tthe start date is bigger than the end date')
-                if  calendar.date <  calendar.adding(days=change_day, months=change_month, years=change_year):
+                if  calendar.date <  calendar.subtracting(days=change_day, months=change_month, years=change_year):
                         print(f'\t\tthe start date is less than the end date')
-                elif calendar.date ==  calendar.adding(days=change_day, months=change_month, years=change_year):
+                elif calendar.date ==  calendar.subtracting(days=change_day, months=change_month, years=change_year):
                     print(f'\t\tThe start date is the same as the end date')
 
 else:
         print("Error, check your choice and try again!")
-
-
-
-
 
